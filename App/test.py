b5df3a505dc82a242667d56dc79cfc9b2df0790b
jincheol <oscar4293@gmail.com>
2024-06-09 21:35:45 +0900

    update

--- App/test.py
+++ App/test.py
@@ -1,11 +1,10 @@
-import torch
+from torch_geometric_temporal.dataset import ChickenpoxDatasetLoader
 from torch_geometric_temporal.signal import temporal_signal_split
-from torch_geometric_temporal.dataset import METRLADatasetLoader
 
-loader = METRLADatasetLoader()
+loader = ChickenpoxDatasetLoader()
+
 dataset = loader.get_dataset()
 
 train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.8)
 
-print(f'Training dataset length: {len(train_dataset)}')
-print(f'Test dataset length: {len(test_dataset)}')
+print("ok")
