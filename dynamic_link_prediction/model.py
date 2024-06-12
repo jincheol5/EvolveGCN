ad7f3a33952f013d5c7d1d878417212aead4d99c
jincheol <oscar4293@gmail.com>
2024-06-12 12:18:16 +0900

    update

--- dynamic_link_prediction/model.py
+++ dynamic_link_prediction/model.py
@@ -1,13 +1,17 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric_temporal.nn.recurrent import EvolveGCNH
+from torch_geometric.nn import GCNConv
 
 
 class EvolveGCN_H_Encoder(torch.nn.Module):
     def __init__(self, num_of_nodes,in_channels):
         super().__init__()
-        self.recurruent = EvolveGCNH(num_of_nodes,in_channels) 
+        self.recurruent = EvolveGCNH(num_of_nodes,in_channels)
+        self.conv1= GCNConv(in_channels,in_channels)
 
     def forward(self, x, edge_index, edge_weight=None):
         x = self.recurruent(x, edge_index, edge_weight)
-        return F.relu(x)
+        x = F.relu(x)
+        x = self.conv1(x,edge_index)
+        return x
