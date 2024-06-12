d9bc6809d99fb5ff38d8cf026de7baefee2efe7a
jincheol <oscar4293@gmail.com>
2024-06-12 13:44:47 +0900

    update

--- dynamic_link_prediction/model.py
+++ dynamic_link_prediction/model.py
@@ -5,12 +5,13 @@ from torch_geometric.nn import GCNConv
 from compute_tR import get_tR_matrix
 from torch_geometric_temporal.dataset import WikiMathsDatasetLoader
 
+# move to GPU (if available)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 loader = WikiMathsDatasetLoader()
 dataset = loader.get_dataset()
 snapshot=dataset[0]
-
-# move to GPU (if available)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tR_indices=get_tR_matrix(snapshot).to(device)
 
 class EvolveGCN_H_Encoder(torch.nn.Module):
     def __init__(self, num_of_nodes,in_channels):
@@ -21,5 +22,5 @@ class EvolveGCN_H_Encoder(torch.nn.Module):
     def forward(self, x, edge_index, edge_weight=None):
         x = self.recurruent(x, edge_index, edge_weight)
         x = F.relu(x)
-        x = self.conv1(x,get_tR_matrix(snapshot).to(device))
+        x = self.conv1(x,tR_indices)
         return x
