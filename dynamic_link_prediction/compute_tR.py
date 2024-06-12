e16f04edf341a85e2d1b6e29d1d501062dd8cfb5
jincheol <oscar4293@gmail.com>
2024-06-12 13:27:54 +0900

    update

--- dynamic_link_prediction/compute_tR.py
+++ dynamic_link_prediction/compute_tR.py
@@ -2,37 +2,31 @@ import networkx as nx
 import numpy as np
 from scipy.sparse import coo_matrix
 from torch_geometric_temporal.dataset import WikiMathsDatasetLoader
+import torch
 
 
-# load dataset
-loader = WikiMathsDatasetLoader()
-dataset = loader.get_dataset()
 
-snapshot=dataset[0]
 
-print(snapshot.x.shape[0])
+def get_tR_matrix(snapshot):
 
-# edge_index로부터 NetworkX 그래프 생성
-edge_index = snapshot.edge_index.numpy()
-G = nx.DiGraph()
-G.add_edges_from(edge_index.T)
+    # edge_index로부터 NetworkX 방향 그래프 생성
+    edge_index = snapshot.edge_index.numpy()
+    G = nx.DiGraph()
+    G.add_edges_from(edge_index.T)
 
-# 강하게 연결된 그래프인지 확인
-is_strongly_connected = nx.is_strongly_connected(G)
-print("is connected?", is_strongly_connected)
+    # # 강하게 연결된 그래프인지 확인
+    # is_strongly_connected = nx.is_strongly_connected(G)
+    # print("is connected?", is_strongly_connected)
 
-# NetworkX 그래프에서 도달 가능성 행렬 계산
-reachability_matrix = nx.floyd_warshall_numpy(G)
-reachability_matrix[reachability_matrix != np.inf] = 1
-reachability_matrix[reachability_matrix == np.inf] = 0
+    # NetworkX 그래프에서 도달 가능성 행렬 계산
+    reachability_matrix = nx.floyd_warshall_numpy(G)
+    reachability_matrix[reachability_matrix != np.inf] = 1
+    reachability_matrix[reachability_matrix == np.inf] = 0
 
-# 도달 가능성 행렬을 COO 형식의 희소 행렬로 변환
-coo_reachability_matrix = coo_matrix(reachability_matrix)
+    # 도달 가능성 행렬을 COO 형식의 희소 행렬로 변환
+    coo_reachability_matrix = coo_matrix(reachability_matrix)
 
-# 결과 출력
-print("COO 형식의 도달 가능성 행렬:")
-print("data:", coo_reachability_matrix.data)
-print("row indices:", coo_reachability_matrix.row)
-print("col indices:", coo_reachability_matrix.col)
+    # 인덱스들을 LongTensor로 변환
+    coo_tensor = torch.LongTensor([coo_reachability_matrix.row, coo_reachability_matrix.col])
 
-print(len(coo_reachability_matrix.row))
+    return coo_tensor
--- dynamic_link_prediction/model.py
+++ dynamic_link_prediction/model.py
@@ -2,6 +2,12 @@ import torch
 import torch.nn.functional as F
 from torch_geometric_temporal.nn.recurrent import EvolveGCNH
 from torch_geometric.nn import GCNConv
+from compute_tR import get_tR_matrix
+from torch_geometric_temporal.dataset import WikiMathsDatasetLoader
+
+loader = WikiMathsDatasetLoader()
+dataset = loader.get_dataset()
+snapshot=dataset[0]
 
 
 class EvolveGCN_H_Encoder(torch.nn.Module):
@@ -13,5 +19,5 @@ class EvolveGCN_H_Encoder(torch.nn.Module):
     def forward(self, x, edge_index, edge_weight=None):
         x = self.recurruent(x, edge_index, edge_weight)
         x = F.relu(x)
-        x = self.conv1(x,edge_index)
+        x = self.conv1(x,get_tR_matrix(snapshot))
         return x
